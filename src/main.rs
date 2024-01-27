af49f05c68b110a327e9c10c655049e3505fb72e
Ludvig Källqvist Nygren <lukecastellan165@gmail.com>
2024-01-27 19:38:13 +0100

    fix: serving empty route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().route("/", get(index_page));
+
+    let addr = SocketAddr::from(([127,0,0,1], 4050));
+
+    println!("Listening to address: {}", addr);
+
+    let listener = TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, router).await.unwrap();
+}
+
+async fn index_page() {
 }
