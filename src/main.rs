71eb37a23bc6919e4b6b99d8b7314c24a392d1c5
Cindy Nilsson <cindynilsson@duck.com>
2024-01-27 23:43:28 +0100

    we needed protobuffs aperently

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::net::SocketAddr;
 
-use api;
-use axum::{routing::get, Router};
+use axum::{Router, routing::get};
 use tokio::net::TcpListener;
+
 #[tokio::main]
 async fn main() {
     let router = Router::new().route("/", get(index_page));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 4050));
+    let addr = SocketAddr::from(([127,0,0,1], 4050));
 
     println!("Listening to address: {}", addr);
 
@@ -15,4 +15,5 @@ async fn main() {
     axum::serve(listener, router).await.unwrap();
 }
 
-async fn index_page() {}
+async fn index_page() {
+}
