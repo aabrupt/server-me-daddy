9fd4187e76837403173ad54ded40629b8bd4a6c8
Cindy Nilsson <cindynilsson@duck.com>
2024-01-29 22:51:22 +0100

    fix: starting to develope the reader

--- functions/src/lib.rs
+++ functions/src/lib.rs
@@ -4,33 +4,29 @@ use parking_lot::{lock_api::RwLockReadGuard, FairMutex, RawRwLock, RwLock};
 use std::{
     collections::HashMap,
     process::{Child, ChildStderr, ChildStdin, ChildStdout, Stdio},
+    rc::Rc,
     sync::Arc,
 };
+use system_child_io::SystemChildIO;
 mod system_child_io;
 use crate::error::SystemError;
-use system_child_io::ReadSystemChildIO;
+
 mod args;
 mod error;
 mod flags;
 /// diffrent types of children one has the IO and one has not so if you wanna fuck with the prosses it is slower than just reading stdin or stdout or stderr
 struct SystemChild(Child);
-struct SystemChildIO {
-    stdin: Option<ChildStdin>,
-    stout: Option<ChildStdout>,
-    stderr: Option<ChildStderr>,
-}
 pub struct SystemManager {
-    running_childprosesses: Arc<FairMutex<HashMap<SystemCommand, SystemChild>>>,
-    running_child_io: Arc<RwLock<HashMap<SystemCommand, RwLock<SystemChildIO>>>>,
+    running_childprosesses: Arc<RwLock<HashMap<SystemCommand, Rc<RwLock<SystemChild>>>>>,
 }
 
 impl SystemManager {
     fn execute(&mut self, syscommand: &SystemCommand) -> Result<(), error::SystemError> {
         use std::process::Command;
-        if self.running_childprosesses.lock().contains_key(&syscommand) {
+        if self.running_childprosesses.read().contains_key(&syscommand) {
             return Err(error::SystemError::AlreadySpawned);
         }
-        let mut child = Command::new(syscommand.path.clone())
+        let child = Command::new(syscommand.path.clone())
             .args(syscommand.args.clone())
             .args(syscommand.flags.clone())
             .stderr(Stdio::piped())
@@ -38,23 +34,17 @@ impl SystemManager {
             .stdin(Stdio::piped())
             .spawn()
             .map_err(|v| SystemError::IoError(v))?;
-        let child_io = SystemChildIO {
-            stdin: child.stdin.take(),
-            stout: child.stdout.take(),
-            stderr: child.stderr.take(),
-        };
+
         self.running_childprosesses
-            .lock()
-            .insert(syscommand.clone(), SystemChild(child));
-        self.running_child_io
             .write()
-            .insert(syscommand.clone(), RwLock::new(child_io));
+            .insert(syscommand.clone(), Rc::new(RwLock::new(SystemChild(child))));
         Ok(())
     }
-    pub fn read_child_io<'a>(&'a self, systemcmd: &SystemCommand) -> Option<ReadSystemChildIO> {
-        let lock: RwLockReadGuard<'a, RawRwLock, HashMap<SystemCommand, RwLock<SystemChildIO>>> =
-            self.running_child_io.read();
-        lock.get(systemcmd).map(move |v| v.read())
+    pub fn read_child_io(&self, systemcmd: &SystemCommand) -> Option<SystemChildIO> {
+        self.running_childprosesses
+            .read()
+            .get(systemcmd)
+            .map(SystemChildIO::new)
     }
 }
 type Path = String;
